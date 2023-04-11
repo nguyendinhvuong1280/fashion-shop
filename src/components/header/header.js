585be70c79a6789d535945f5ede1144e78b730a7
vuongnd848 <vuongnd@edsolabs.com>
2023-04-11 18:39:01 +0700

    add header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,28 @@
-import SearchHeader from "./search";
+import { ActionIcon, Flex, Input } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
 
 function Header() {
     return (
-        <div className="Header" style={{ display: "flex" }}>
-            <div style={{backgroundColor: "#666666"}}>
-                Hotline đặt hàng
-            </div>
-            <div>
-                Tra cứu đơn hàng
-            </div>
-            <div>
-                Hệ thống Showroom
-            </div>
-            <SearchHeader></SearchHeader>
-        </div>
+        <Flex h={50} align='center' style={{ background: "#339AF0" }} justify='center'>
+            <Flex w={1434} justify="space-between">
+                <Flex>Hotline đặt hàng 18006013</Flex>
+                <Flex align='center'>
+                    <div to="#">Tra cứu tình trạng đơn hàng</div>
+                    <div to="#">Hệ thống Showroom</div>
+                    <Input
+                        w={313}
+                        style={{ background: "none" }}
+                        rightSection={
+                            <ActionIcon radius="xl" >
+                                <IconSearch />
+                            </ActionIcon>
+                        }
+                        placeholder="Tìm kiếm sản phẩm"
+                        radius="xl"
+                    />
+                </Flex>
+            </Flex>
+        </Flex>
     )
 }
 
